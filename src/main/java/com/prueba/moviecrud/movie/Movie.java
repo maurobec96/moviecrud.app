8a782b9e727239071071669f70abdf735f033364
Mauricio Espinosa <mauricio.bec1996@gmail.com>
2023-10-07 09:32:35 -0400

    Small fix

--- src/main/java/com/prueba/moviecrud/movie/Movie.java
+++ src/main/java/com/prueba/moviecrud/movie/Movie.java
@@ -20,7 +20,7 @@ import jakarta.persistence.ManyToOne;
 public class Movie {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ID_MOVIE")
     private Long id;
 
